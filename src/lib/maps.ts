82bf4c04d43e7ef9a44a58c687631a696769dbd9
lucioreyli <lucioandradejr@gmail.com>
2026-01-26 19:36:15 -0400

    chore: update deps

--- src/lib/maps.ts
+++ src/lib/maps.ts
@@ -17,7 +17,22 @@ export const normalizeType = (mapType: number): string => {
     if (typeof mt === 'number' && mapType & mt) {
       return `${acc}_${key}`;
     }
-
     return acc;
   }, 'TUNNEL');
+};
+
+type ResourceK = `r${'F' | 'H' | 'O' | 'R' | 'W'}`;
+type ChestK = `c${'G' | 'B' | 'HG' | 'LG'}`;
+type DungeonK = `d${'S' | 'G' | 'A'}`;
+
+type MapData = Partial<Record<ChestK | DungeonK | ResourceK, number>>;
+
+export const parseMapData = (data: string): MapData => {
+  // "cB1,cLG1,dS1,dG1,rO3,rW1"
+
+  data.split(',').reduce((acc: MapData, rawData: string) => {
+    return acc;
+  }, {});
+
+  return {};
 };
