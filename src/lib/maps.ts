e7d8874f2b108b222b57af77e87740052387eea6
lucioreyli <lucioandradejr@gmail.com>
2026-01-26 18:54:19 -0400

    refactor: render map type

--- src/lib/maps.ts
+++ src/lib/maps.ts
@@ -1,4 +1,16 @@
-import { MAP_TYPES } from '@/constants/map-types';
+export enum MAP_TYPES {
+  ROYAL = 1 << 0,
+  RED = 1 << 1,
+  BLACK = 1 << 2,
+  HIDEOUT = 1 << 3,
+
+  LOW = 1 << 4,
+  MEDIUM = 1 << 5,
+  HIGH = 1 << 6,
+  DEEP = 1 << 7,
+
+  RAID = 1 << 8,
+}
 
 export const normalizeType = (mapType: number): string => {
   return Object.entries(MAP_TYPES).reduce((acc, [key, mt]) => {
